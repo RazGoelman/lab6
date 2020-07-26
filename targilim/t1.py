639100968f16d3f3dbbadcf089811d77ab314391
migrgoel <goelmanraz@gmail.com>
2020-07-26 22:23:35 +0300

    first net4u

---
+++ targilim/t1.py
@@ -0,0 +1,8 @@
+
+# Math
+num = 4567
+num1 = 4567//1000
+num2 = ((4567%1000)//100)
+num3 = (4567%100)//10
+num4 = 4567%10
+print("4567 is: ""\nalafim: " + str(num1) + "\nmeot: " + str(num2) + "\nasarot: " + str(num3) + "\nahadot: " + str(num4))
